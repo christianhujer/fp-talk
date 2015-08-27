fc416198fe51f6a7aea772e45d886381f5e9baeb
Christian Hujer <Christian.Hujer@gmail.com>
2015-08-27 11:54:32 +0530

    Add some slides about functional programming in C and improve the C examples to fit on slides.

--- src/echo.c
+++ src/echo.c
@@ -1,20 +1,13 @@
 // echo with recursion in C
-
 #include <stdio.h>
 
-static void echo(char *argv[])
-{
-    if (!argv[0]) {
-        printf("\n");
-        return; // end recursion
-    }
-    printf("%s", argv[0]);
-    if (argv[1]) printf(" ");
+static void echo(char *argv[]) {
+    if (!argv[0]) { printf("\n"); return; } // stop
+    printf(argv[1] ? "%s " : "%s", argv[0]);
     echo(argv + 1); // recurse
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     ++argv, --argc; // skip command name
     echo(argv);
 }
